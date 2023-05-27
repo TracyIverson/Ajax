119c2028d4df88e96a799963249e496e03d10d77
Tracy Iverson <1797265543@qq.com>
2023-05-27 23:23:51 +0800

    添加post请求路由

--- 2-原生Ajax/server.js
+++ 2-原生Ajax/server.js
@@ -7,11 +7,19 @@ const app = express();
 //3.创建路由规则
 //request是对请求报文的封装
 //response是对响应报文的封装
-app.get('/', (request, response) => {
+app.get('/server', (request, response) => {
     //设置响应头允许跨域
     response.setHeader("Access-Control-Allow-Origin", "*");
     //设置响应
-    response.send("Hello Express");
+    response.send("get");
+})
+
+//post请求
+app.post('/server', (request, response) => {
+    //设置响应头允许跨域
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    //设置响应
+    response.send("post");
 })
 
 //4.监听端口启动服务
