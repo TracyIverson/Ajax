1386f67398a4d8b95baccaeef7c13e6d4c834f42
Tracy Iverson <1797265543@qq.com>
2023-05-28 16:19:44 +0800

    请求 JSON 数据

--- 2-原生Ajax/server.js
+++ 2-原生Ajax/server.js
@@ -22,6 +22,20 @@ app.post('/server', (request, response) => {
     response.send("post");
 })
 
+//获取JSON数据
+app.all('/json', (request, response) => {
+    //设置响应头允许跨域
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    //新建一个student对象
+    let student = {
+        name: "Jack",
+        age: 18,
+    }
+    //序列化对象
+    let jsonStr = JSON.stringify(student);
+    //设置响应
+    response.send(jsonStr);
+})
 //4.监听端口启动服务
 app.listen(8000, () => {
     console.log("服务已经启动，8000 端口监听中....")
